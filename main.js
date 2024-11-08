ab794e54bb33ec6eddc8402635b931e3a746e552
Vadim4k-nya <filonovadim11education@gmail.com>
2024-11-08 20:59:39 +0500

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var counter = 0
-
-const onClick = () => {
-    document.getElementById('counter').innerText = counter + 1;
-    counter++;
-}
-
-document.getElementById('topcoin').addEventListener('click', onClick)
